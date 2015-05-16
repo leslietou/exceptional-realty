12bf8010e9539ee9bc7213f45efe4abd86123e52
leslie-tou <leslie@fashiongps.com>
2015-05-16 16:03:42 -0400

    add PHP Mailer

--- contact.php
+++ contact.php
@@ -29,7 +29,7 @@
         <div class="wrapper">
             <nav class="clearfix">
               <a class="menu-icon">&#9776;</a>
-              <div id="menu-links"><a href="index.html">About</a><a href="new-properties.html">New Properties</a><a href="real-estate-listings.html">Listings</a><a href="market-report.html">Market Report</a><a class="selected" href="contact.html">Contact</a><a href="http://hud.gov" target="_blank">H.U.D.</a></div>
+              <div id="menu-links"><a href="index.html">About</a><a href="new-properties.html">New Properties</a><a href="real-estate-listings.html">Listings</a><a href="market-report.html">Market Report</a><a class="selected" href="contact.php">Contact</a><a href="http://hud.gov" target="_blank">H.U.D.</a></div>
             </nav>
         </div><!-- .wrapper -->
       </div><!-- #navbar -->
@@ -38,6 +38,14 @@
     <div class="wrapper clearfix">
 
       <section class="col-2 first shadow">
+        
+      <?php if ($_GET['s']=='success'){ ?> 
+      <p> Thank you, your message has been sent successfully!</p>
+
+      <?php } else if($_GET['s']== 'error'){ ?>
+      <p> Make sure you type in the proper verification numbers.<a href="contact.php"> Click here to try again </a>. If issues persist please send an email to the site admin. (admin@site.com) </p>
+
+      <?php } else { ?>
         <!-- Contact Form -->
         <form action="#" method="post">
 
@@ -54,10 +62,18 @@
           
           <label for="message">Message</label>
           <span id="message-error" class="error">can not be left blank.</span>
-          <textarea id="message" name="message" rows="10"></textarea>
+          <textarea id="message" name="message" rows="8"></textarea>
           
+          <input type="text" name="verify" class="verify-box" placeholder="Verify you're human">
+          
+          <img class="verify-image" src="verificationimage.php?<?php echo rand(0,9999) ?>" alt="verification image">
+
           <input type="submit" value="send">
+
         </form>
+
+        <?php} ?>
+
       </section>
 
       <section class="col-1 shadow">
